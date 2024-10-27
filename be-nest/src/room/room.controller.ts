20ba201db3ae854cddc04b028165d76dcf885a84
Felix <jonfelixrico@gmail.com>
2024-10-27 14:59:45 +0800

    Add create endpoint

--- be-nest/src/room/room.controller.ts
+++ be-nest/src/room/room.controller.ts
@@ -4,6 +4,7 @@ import {
   HttpException,
   HttpStatus,
   Param,
+  Post,
 } from '@nestjs/common'
 import { RoomService } from 'src/room/room.service/room.service'
 
@@ -12,11 +13,20 @@ export class RoomController {
   constructor(private svc: RoomService) {}
 
   @Get(':id')
-  getId(@Param('id') id: string) {
+  checkIfExists(@Param('id') id: string) {
     if (this.svc.exists(id)) {
       return
     }
 
     throw new HttpException('Not found', HttpStatus.NOT_FOUND)
+  }
+
+  @Post()
+  create() {
+    const roomId = this.svc.create()
+
+    return {
+      roomId,
+    }
   }
 }
