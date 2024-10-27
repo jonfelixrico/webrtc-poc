a3c2076710df7b787634fa321f526823e8327d10
Felix <jonfelixrico@gmail.com>
2024-10-27 14:58:19 +0800

    Add get operation

--- be-nest/src/room/room.controller.ts
+++ be-nest/src/room/room.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common'
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+} from '@nestjs/common'
+import { RoomService } from 'src/room/room.service/room.service'
 
 @Controller('room')
-export class RoomController {}
+export class RoomController {
+  constructor(private svc: RoomService) {}
+
+  @Get(':id')
+  getId(@Param('id') id: string) {
+    if (this.svc.exists(id)) {
+      return
+    }
+
+    throw new HttpException('Not found', HttpStatus.NOT_FOUND)
+  }
+}
