811cca600aeed6b3031c2fbbf0a8c1eefc1186a2
Felix <jonfelixrico@gmail.com>
2024-10-27 22:17:16 +0800

    Add imports in CMediaStreamRenderer

--- fe-nuxt/components/CMediaStreamRenderer.vue
+++ fe-nuxt/components/CMediaStreamRenderer.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { toValue, useTemplateRef, watch, type PropType } from 'vue'
+import { computed, toValue, useTemplateRef, watch, type PropType } from 'vue'
 
 const props = defineProps({
   mediaStream: {
