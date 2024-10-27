2f7efe37452c8b592e4c01715e3224fd9f3403af
Felix <jonfelixrico@gmail.com>
2024-10-27 14:06:44 +0800

    Remove remaining traces of Quasar

--- fe-nuxt/pages/index.vue
+++ fe-nuxt/pages/index.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
 const callerId = useCallerId()
 const targerCallerId = ref()
 </script>
 
 <template>
-  <q-page>
+  <div>
     {{ callerId }}
 
     <form @submit.prevent>
       <UInput v-model="targerCallerId" />
       <UButton type="submit">Call</UButton>
     </form>
-  </q-page>
+  </div>
 </template>
