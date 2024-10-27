a7af5ea67f85a0dae8f7d5e7a205b15334ee7756
Felix <jonfelixrico@gmail.com>
2024-10-27 13:30:17 +0800

    Copy coponents from the Quasar projects to the Nuxt project

--- fe-nuxt/pages/index.vue
+++ fe-nuxt/pages/index.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
-const { t } = useI18n()
+import { useCallerId } from 'src/composables/caller-id.composable'
+import { ref } from 'vue'
+
+const callerId = useCallerId()
+const targerCallerId = ref()
 </script>
 
 <template>
-  <div class="h-dvh w-dvh flex flex-row justify-center items-center">
-    <UCard>
-      <div class="text-2xl font-medium">
-        {{ t('common.helloWorld') }}
-      </div>
-    </UCard>
-  </div>
+  <q-page>
+    {{ callerId }}
+
+    <q-form @submit.prevent>
+      <q-input v-model="targerCallerId" />
+      <q-btn type="submit">Call</q-btn>
+    </q-form>
+  </q-page>
 </template>
