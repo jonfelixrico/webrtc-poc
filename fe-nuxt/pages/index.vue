581baffe4bd890dbf07a1c557fcf9a770386f46d
Felix <jonfelixrico@gmail.com>
2024-10-27 17:38:35 +0800

    Adjust the index page

--- fe-nuxt/pages/index.vue
+++ fe-nuxt/pages/index.vue
@@ -1,15 +1,41 @@
 <script setup lang="ts">
-const callerId = useCallerId()
-const targerCallerId = ref()
+const roomJoinModel = ref()
+const router = useRouter()
+
+function goToRoom(roomId: string) {
+  router.push({
+    name: 'rooms-id',
+    params: {
+      id: roomId,
+    },
+  })
+}
+
+async function createRoom() {
+  // TODO proxy the BE to be under /be
+  const { roomId } = await $fetch<{ roomId: string }>(
+    'http://localhost:3050/rooms',
+    {
+      method: 'POST',
+    },
+  )
+
+  goToRoom(roomId)
+}
 </script>
 
 <template>
-  <div>
-    {{ callerId }}
-
-    <form @submit.prevent>
-      <UInput v-model="targerCallerId" />
+  <div class="flex flex-row gap-4 w-dvw h-dvh">
+    <form
+      @submit.prevent="goToRoom(roomJoinModel)"
+      class="flex-1 flex flex-row items-center justify-center gap-2"
+    >
+      <UInput v-model="roomJoinModel" />
       <UButton type="submit">Call</UButton>
     </form>
+
+    <div class="flex-1 flex flex-row items-center justify-center">
+      <UButton @click="createRoom">Create a Room</UButton>
+    </div>
   </div>
 </template>
