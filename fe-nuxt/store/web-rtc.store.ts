4f252bb0837c33fbd83ca7c9bc8b3d6e8e6e935e
Felix <jonfelixrico@gmail.com>
2024-10-29 18:06:46 +0800

    Add map for tracking connection statuses

--- fe-nuxt/store/web-rtc.store.ts
+++ fe-nuxt/store/web-rtc.store.ts
@@ -2,11 +2,13 @@ import { defineStore } from 'pinia'
 
 export interface WebRtcStore {
   peerConnections: Record<string, RTCPeerConnection>
+  connectionStatuses: Record<string, 'connected'>
 }
 
 export const useWebRtcStore = defineStore('webRtc', {
   state: () =>
     ({
       peerConnections: {},
+      connectionStatuses: {},
     }) as WebRtcStore,
 })
