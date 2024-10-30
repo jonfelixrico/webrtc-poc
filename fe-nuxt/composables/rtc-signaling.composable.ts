0bd5d3dc784a72532c1b86c08b42b8a8ec9828c7
Felix <jonfelixrico@gmail.com>
2024-10-30 19:29:19 +0800

    Add TURN server

--- fe-nuxt/composables/rtc-signaling.composable.ts
+++ fe-nuxt/composables/rtc-signaling.composable.ts
@@ -9,16 +9,11 @@ import { useWebRtcStore } from '~/store/web-rtc.store'
 import { makeConnectionReactive } from '~/utils/rtc.util'
 
 const ICE_SERVERS: RTCIceServer[] = [
-  { urls: 'stun:stun.l.google.com:19302' },
-  { urls: 'stun:stun.l.google.com:5349' },
-  { urls: 'stun:stun1.l.google.com:3478' },
-  { urls: 'stun:stun1.l.google.com:5349' },
-  { urls: 'stun:stun2.l.google.com:19302' },
-  { urls: 'stun:stun2.l.google.com:5349' },
-  { urls: 'stun:stun3.l.google.com:3478' },
-  { urls: 'stun:stun3.l.google.com:5349' },
-  { urls: 'stun:stun4.l.google.com:19302' },
-  { urls: 'stun:stun4.l.google.com:5349' },
+  {
+    urls: 'turn:coturn.jfrapps.com:3478',
+    username: 'turn',
+    credential: 'turn',
+  },
 ]
 
 export function useRtcJoinHandler(
