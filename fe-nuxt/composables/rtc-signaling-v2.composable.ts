d5afe03c0a2544ba0d39b3fad53b4244d46b8001
Felix <jonfelixrico@gmail.com>
2024-11-01 00:28:38 +0800

    Add new offer listener

--- fe-nuxt/composables/rtc-signaling-v2.composable.ts
+++ fe-nuxt/composables/rtc-signaling-v2.composable.ts
@@ -51,6 +51,64 @@ export function useCreateConnection() {
   return createConnection
 }
 
+export function useNewOfferListener() {
+  const logger = useLogger()
+
+  const store = useWebRtcStore()
+  const socket = useSocketFromStore()
+
+  const existingClientIds = computed(
+    () => new Set(Object.keys(store.$state.connections)),
+  )
+
+  onSocketEvent(
+    'offer_sent',
+    async ({
+      clientId,
+      rtcSession,
+    }: {
+      clientId: string
+      rtcSession: RTCSessionDescriptionInit
+    }) => {
+      if (toValue(existingClientIds).has(clientId)) {
+        return
+      }
+
+      logger.info('Received offer from client %s', clientId)
+
+      const conn = new RTCPeerConnection({
+        iceServers: ICE_SERVERS,
+        iceTransportPolicy: 'relay',
+      })
+
+      function emitCandidate({ candidate }: RTCPeerConnectionIceEvent) {
+        toValue(socket).emit('send_ice_candidate', {
+          clientId,
+          iceCandidate: candidate,
+        })
+      }
+      conn.addEventListener('icecandidate', emitCandidate)
+
+      try {
+        await conn.setRemoteDescription(new RTCSessionDescription(rtcSession))
+        const answer = await conn.createAnswer()
+        await conn.setLocalDescription(answer)
+
+        toValue(socket).emit('accept_offer', {
+          rtcSession: answer,
+          clientId,
+        })
+        logger.info('Sent offer acceptance to client %s', clientId)
+
+        const reactiveConn = makeConnectionReactive(conn)
+        store.$state.connections[clientId] = reactiveConn
+      } finally {
+        conn.removeEventListener('icecandidate', emitCandidate)
+      }
+    },
+  )
+}
+
 export function useNegotiationHandlers(
   peerConnection: RTCPeerConnection,
   peerClientId: string,
@@ -100,16 +158,11 @@ export function useNegotiationHandlers(
 
       logger.info('Received offer from client %s', clientId)
 
-      const conn = new RTCPeerConnection({
-        iceServers: ICE_SERVERS,
-        iceTransportPolicy: 'relay',
-      })
-      const reactiveConn = makeConnectionReactive(conn)
-      connObj.value = reactiveConn
-
-      await conn.setRemoteDescription(new RTCSessionDescription(rtcSession))
-      const answer = await conn.createAnswer()
-      await conn.setLocalDescription(answer)
+      await peerConnection.setRemoteDescription(
+        new RTCSessionDescription(rtcSession),
+      )
+      const answer = await peerConnection.createAnswer()
+      await peerConnection.setLocalDescription(answer)
 
       toValue(socket).emit('accept_offer', {
         rtcSession: answer,
