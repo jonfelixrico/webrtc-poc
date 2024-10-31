6dbb138058bc7ee957e0fc3340e9e62adf02f26e
Felix <jonfelixrico@gmail.com>
2024-10-31 22:10:50 +0800

    Create useStreamSender

---
+++ fe-nuxt/composables/rtc-media.composable.ts
@@ -0,0 +1,25 @@
+import { watch } from 'vue'
+import { useMediaStreamStore } from '~/store/media-stream.store'
+import { useLogger } from '~/composables/logger.composable'
+
+export function useStreamSender(peerConnection: RTCPeerConnection) {
+  const { $state } = useMediaStreamStore()
+  const logger = useLogger()
+
+  watch(
+    () => $state.mediaStream,
+    (stream) => {
+      if (!stream) {
+        return
+      }
+
+      for (const track of stream.getTracks()) {
+        try {
+          peerConnection.addTrack(track, stream)
+        } catch (e) {
+          logger.warn('Error encountered while adding track %s', track.id, e)
+        }
+      }
+    },
+  )
+}
