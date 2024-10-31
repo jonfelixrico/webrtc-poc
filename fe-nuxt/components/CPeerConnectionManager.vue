693c43a845e4d2e1911894f251af202a5baa31f5
Felix <jonfelixrico@gmail.com>
2024-10-31 22:03:11 +0800

    Use the composables

--- fe-nuxt/components/CPeerConnectionManager.vue
+++ fe-nuxt/components/CPeerConnectionManager.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
 import type { PropType } from 'vue'
 import {
-  onSocketEvent,
-  useSocketFromStore,
-} from '~/composables/socket-v2.composable'
-import { useLogger } from '~/composables/logger.composable'
-import { ICE_SERVERS } from '~/typings/ice-servers.const'
-import { computed, makeConnectionReactive, toValue } from '#imports'
-import { useWebRtcStore } from '~/store/web-rtc.store'
+  useIceCandidateHandlers,
+  useNegotiationHandlers,
+} from '~/composables/rtc-signaling-v2.composable'
 
 const props = defineProps({
   peerConnection: {
@@ -21,100 +17,6 @@ const props = defineProps({
   },
 })
 
-const logger = useLogger()
-const store = useWebRtcStore()
-
-const socket = useSocketFromStore()
-
-const connObj = computed({
-  get: () => store.$state.connections[props.peerClientId],
-
-  set: (val) => {
-    store.$state.connections[props.peerClientId] = val
-  },
-})
-
-onSocketEvent(
-  'offer_accepted',
-  ({
-    clientId,
-    rtcSession,
-  }: {
-    clientId: string
-    rtcSession: RTCSessionDescriptionInit
-    roomId: string
-  }) => {
-    if (clientId !== props.peerClientId) {
-      return
-    }
-
-    const { peerConnection } = props
-    peerConnection.setRemoteDescription(new RTCSessionDescription(rtcSession))
-    logger.info('Completed handshake with client %s', clientId)
-  },
-)
-
-onSocketEvent(
-  'offer_sent',
-  async ({
-    clientId,
-    rtcSession,
-    roomId,
-  }: {
-    clientId: string
-    rtcSession: RTCSessionDescriptionInit
-    roomId: string
-  }) => {
-    if (clientId !== props.peerClientId) {
-      return
-    }
-
-    logger.info('Received offer from client %s', clientId)
-
-    const conn = new RTCPeerConnection({
-      iceServers: ICE_SERVERS,
-      iceTransportPolicy: 'relay',
-    })
-    const reactiveConn = makeConnectionReactive(conn)
-    connObj.value = reactiveConn
-
-    await conn.setRemoteDescription(new RTCSessionDescription(rtcSession))
-    const answer = await conn.createAnswer()
-    await conn.setLocalDescription(answer)
-
-    toValue(socket).emit('accept_offer', {
-      roomId,
-      rtcSession: answer,
-      clientId,
-    })
-
-    logger.info('Sent offer acceptance to client %s', clientId)
-  },
-)
-
-onSocketEvent(
-  'ice_candidate_sent',
-  async ({
-    clientId,
-    iceCandidate,
-  }: {
-    clientId: string
-    iceCandidate: RTCIceCandidateInit
-    roomId: string
-  }) => {
-    if (clientId !== props.peerClientId) {
-      return
-    }
-
-    const { peerConnection } = props
-
-    logger.debug('Incoming ice candidate from client %s...', clientId)
-    try {
-      await peerConnection.addIceCandidate(iceCandidate)
-      logger.info('Added ice candidate from client %s', clientId)
-    } catch (e) {
-      logger.warn('Failed adding ice candidate from client %s', clientId)
-    }
-  },
-)
+useNegotiationHandlers(props.peerConnection, props.peerClientId)
+useIceCandidateHandlers(props.peerConnection, props.peerClientId)
 </script>
