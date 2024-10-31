64294caada5b8bec83872460c898933e4b723290
Felix <jonfelixrico@gmail.com>
2024-10-31 22:11:25 +0800

    Add the stream sender

--- fe-nuxt/components/CPeerConnectionManager.vue
+++ fe-nuxt/components/CPeerConnectionManager.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { PropType } from 'vue'
+import { useStreamSender } from '~/composables/rtc-media.composable'
 import {
   useIceCandidateHandlers,
   useNegotiationHandlers,
@@ -19,4 +20,5 @@ const props = defineProps({
 
 useNegotiationHandlers(props.peerConnection, props.peerClientId)
 useIceCandidateHandlers(props.peerConnection, props.peerClientId)
+useStreamSender(props.peerConnection)
 </script>
