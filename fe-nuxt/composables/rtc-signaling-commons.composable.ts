98a0279841668d2b55d75cfe547b2689e6839bb5
Felix <jonfelixrico@gmail.com>
2024-11-01 16:48:45 +0800

    Relocate sendOffer

--- fe-nuxt/composables/rtc-signaling-commons.composable.ts
+++ fe-nuxt/composables/rtc-signaling-commons.composable.ts
@@ -2,30 +2,6 @@ import { onBeforeUnmount, toValue } from 'vue'
 import { useLogger } from '~/composables/logger.composable'
 import { useSocketFromStore } from '~/composables/socket-v2.composable'
 
-export function useSendOffer() {
-  const logger = useLogger()
-  const socket = useSocketFromStore()
-
-  async function sendOffer(
-    peerConnection: RTCPeerConnection,
-    peerClientId: string,
-  ) {
-    const offer = await peerConnection.createOffer({
-      offerToReceiveAudio: true,
-      offerToReceiveVideo: true,
-    })
-    await peerConnection.setLocalDescription(offer)
-
-    toValue(socket).emit('send_offer', {
-      clientId: peerClientId,
-      rtcSession: offer,
-    })
-    logger.debug('Sent an offer to client %s', peerClientId)
-  }
-
-  return sendOffer
-}
-
 export function useAddListener(connection: RTCPeerConnection) {
   const unsubscribeFns: ((...args: any[]) => void)[] = []
 
