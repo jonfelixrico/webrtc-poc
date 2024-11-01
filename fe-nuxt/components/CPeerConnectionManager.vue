fec6cb4f68610f0cf69c591911f5aa95e3eb3cd3
Felix <jonfelixrico@gmail.com>
2024-11-01 18:45:42 +0800

    Use the composable

--- fe-nuxt/components/CPeerConnectionManager.vue
+++ fe-nuxt/components/CPeerConnectionManager.vue
@@ -6,6 +6,7 @@ import {
 } from '~/composables/rtc-media.composable'
 import {
   useCandidateHandlers,
+  useNegotiationNeededHandler,
   useStatesListeners,
 } from '~/composables/rtc-signaling-individual.composable'
 import { useLogger } from '~/composables/logger.composable'
@@ -26,6 +27,7 @@ useCandidateHandlers(props.peerConnection, props.peerClientId)
 useStreamSender(props.peerClientId)
 useStreamReceiver(props.peerConnection, props.peerClientId)
 useStatesListeners(props.peerConnection, props.peerClientId)
+useNegotiationNeededHandler(props.peerConnection, props.peerClientId)
 
 const logger = useLogger()
 logger.debug('Started manager for client %s', props.peerClientId)
