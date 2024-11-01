d4b413e7817f020340697eca22970ab534ee0785
Felix <jonfelixrico@gmail.com>
2024-11-01 23:20:57 +0800

    Add type maps

--- types/src/index.ts
+++ types/src/index.ts
@@ -3,4 +3,35 @@ export interface RoomWsCommandMap {
     toClient: string
     candidate: RTCIceCandidate
   }
+
+  send_description: {
+    toClient: string
+    description: RTCSessionDescription
+  }
+
+  join: {
+    roomId: string
+  }
+}
+
+export interface RoomWsEventMap {
+  candidate_sent: {
+    fromClient: string
+    candidate: RTCIceCandidate
+  }
+
+  description_sent: {
+    fromClient: string
+    description: RTCIceCandidate
+  }
+
+  user_list_synced: {
+    clientIds: string[]
+    roomId: string
+  }
+
+  user_joined: {
+    clientId: string
+    roomId: string
+  }
 }
