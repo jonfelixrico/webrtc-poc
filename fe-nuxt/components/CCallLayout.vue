3a9313934b9a29c70b33930cb4cf8096c7e314bd
Felix <jonfelixrico@gmail.com>
2024-11-02 19:58:16 +0800

    Impl base of CResizeObserverDiv

---
+++ fe-nuxt/components/CCallLayout.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { PropType } from 'vue'
+import CResizeObserverDiv from '~/components/CResizeObserverDiv.vue'
+
+defineProps({
+  ids: {
+    type: Array as PropType<string[]>,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="flex flex-row">
+    <CResizeObserverDiv>
+      <slot name="self" />
+    </CResizeObserverDiv>
+
+    <CResizeObserverDiv v-for="id in ids" :key="id" v-slot="dimensions">
+      <slot name="peer" :dimensions />
+    </CResizeObserverDiv>
+  </div>
+</template>
