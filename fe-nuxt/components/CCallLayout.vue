6c0cf919b0f26809e3016757339a1f9fb69a01c5
Felix <jonfelixrico@gmail.com>
2024-11-02 20:06:13 +0800

    Use default slot only

--- fe-nuxt/components/CCallLayout.vue
+++ fe-nuxt/components/CCallLayout.vue
@@ -12,12 +12,8 @@ defineProps({
 
 <template>
   <div class="flex flex-row">
-    <CCallLayoutItem v-slot="{ width, height }">
-      <slot name="self" :width :height />
-    </CCallLayoutItem>
-
     <CCallLayoutItem v-for="id in ids" :key="id" v-slot="{ width, height }">
-      <slot name="peer" :width :height />
+      <slot :width :height />
     </CCallLayoutItem>
   </div>
 </template>
