ac38e391908245b93daedbf042ead5771be080a6
Felix <jonfelixrico@gmail.com>
2024-11-02 20:10:26 +0800

    Utilize grid classes

--- fe-nuxt/components/CCallLayout.vue
+++ fe-nuxt/components/CCallLayout.vue
@@ -1,17 +1,29 @@
 <script setup lang="ts">
-import type { PropType } from 'vue'
+import { computed, type PropType } from 'vue'
 import CCallLayoutItem from '~/components/CCallLayoutItem.vue'
 
-defineProps({
+const props = defineProps({
   ids: {
     type: Array as PropType<string[]>,
     required: true,
   },
 })
+
+const gridColsClass = computed(() => {
+  const length = props.ids.length
+
+  if (length <= 1) {
+    return 'grid-cols-1'
+  } else if (length === 2) {
+    return 'grid-cols-2'
+  } else {
+    return 'grid-cols-3'
+  }
+})
 </script>
 
 <template>
-  <div class="flex flex-row">
+  <div class="flex flex-row grid gap-4" :class="gridColsClass">
     <CCallLayoutItem v-for="id in ids" :key="id" v-slot="{ width, height }">
       <slot :width :height />
     </CCallLayoutItem>
