4efdbaf6688d64f47f5f22a19671f6685b4f6d11
Felix <jonfelixrico@gmail.com>
2024-11-02 20:04:28 +0800

    Adjust layout scoped values

--- fe-nuxt/components/CCallLayout.vue
+++ fe-nuxt/components/CCallLayout.vue
@@ -12,12 +12,12 @@ defineProps({
 
 <template>
   <div class="flex flex-row">
-    <CCallLayoutItem>
-      <slot name="self" />
+    <CCallLayoutItem v-slot="{ width, height }">
+      <slot name="self" :width :height />
     </CCallLayoutItem>
 
-    <CCallLayoutItem v-for="id in ids" :key="id" v-slot="dimensions">
-      <slot name="peer" :dimensions />
+    <CCallLayoutItem v-for="id in ids" :key="id" v-slot="{ width, height }">
+      <slot name="peer" :width :height />
     </CCallLayoutItem>
   </div>
 </template>
