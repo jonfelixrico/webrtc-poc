6eb217d9245b804aa34813913b6d03d46a04665f
Felix <jonfelixrico@gmail.com>
2024-11-02 16:41:49 +0800

    Make the toolbar work

--- fe-nuxt/components/CCallToolbar.vue
+++ fe-nuxt/components/CCallToolbar.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useDevicesList, useUserMedia } from '@vueuse/core'
-import { computed, reactive, watch } from 'vue'
+import { computed, ref, toValue, watch } from 'vue'
 import { useMediaStreamStore } from '~/store/media-stream.store'
 import { useLogger } from '~/composables/logger.composable'
 import CDeviceSelect from '~/components/CDeviceSelect.vue'
@@ -14,39 +14,43 @@ const { audioInputs, videoInputs } = useDevicesList({
   requestPermissions: true,
 })
 
-interface DeviceValue {
-  id: string | null
-  enabled: boolean
-}
+const aId = ref<string | null>(null)
+const aEnabled = ref<boolean>(false)
+const vId = ref<string | null>(null)
+const vEnabled = ref<boolean>(false)
 
-const audioV = reactive<DeviceValue>({
-  id: null,
-  enabled: false,
+const constraints = computed<MediaStreamConstraints>(() => {
+  const value: MediaStreamConstraints = {}
+
+  if (toValue(aId) && toValue(aEnabled)) {
+    value.audio = {
+      deviceId: toValue(aId) as string,
+    }
+  }
+
+  if (toValue(vId) && toValue(vEnabled)) {
+    value.video = {
+      deviceId: toValue(vId) as string,
+    }
+  }
+
+  return value
 })
 
-const videoV = reactive<DeviceValue>({
-  id: null,
-  enabled: false,
+watch(constraints, (c) => console.log(c))
+
+const { stream } = useUserMedia({
+  enabled: computed(() => Object.keys(toValue(constraints)).length > 0),
+  constraints,
+  autoSwitch: true,
 })
 
 const msStore = useMediaStreamStore()
-const constraints = reactive<MediaStreamConstraints>({
-  audio: reactive({
-    deviceId: computed(() => audioV.id ?? undefined),
-  }),
-  video: reactive({
-    deviceId: computed(() => videoV.id ?? undefined),
-  }),
-})
-const { stream } = useUserMedia({
-  enabled: true,
-  constraints: constraints,
-})
 watch(stream, (stream) => {
   logger.info(
     'Media stream has changed; video: %s, audio: %s',
-    videoV.id,
-    audioV.id,
+    toValue(vId),
+    toValue(aId),
   )
   msStore.mediaStream = stream ?? null
 })
@@ -55,13 +59,13 @@ watch(stream, (stream) => {
 <template>
   <div class="flex flex-row">
     <CDeviceSelect
-      v-model="audioV.id"
-      v-model:enabled="audioV.enabled"
+      v-model="aId"
+      v-model:enabled="aEnabled"
       :devices="audioInputs"
     />
     <CDeviceSelect
-      v-model="videoV.id"
-      v-model:enabled="videoV.enabled"
+      v-model="vId"
+      v-model:enabled="vEnabled"
       :devices="videoInputs"
     />
   </div>
