a27f4a7b87c2b9d97ff4747bf94fb2c33dc2ced7
Felix <jonfelixrico@gmail.com>
2024-11-11 23:33:23 +0800

    Create wrapper for emit

--- be-nest/src/room/room-ws/room-ws.gateway.ts
+++ be-nest/src/room/room-ws/room-ws.gateway.ts
@@ -24,6 +24,14 @@ function getRoomId(socket: Socket) {
   return ID_EXTRACT_REGEXP.exec(urlString)?.[1] ?? null
 }
 
+function wrapEmitter(
+  emitter: Pick<Socket, 'emit'>,
+) {
+  return <K extends keyof RoomWsEventPayloadMap>(event: K, payload: RoomWsEventPayloadMap[K]) {
+    emitter.emit(event, payload)
+  }
+}
+
 @WebSocketGateway(/^room-(.+)/)
 export class RoomWsGateway implements OnGatewayDisconnect, OnGatewayConnection {
   constructor(private logger: Logger) {}
