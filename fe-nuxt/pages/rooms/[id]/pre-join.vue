90d82c231bd94f2423a45772185666cf5f9de91c
Felix <jonfelixrico@gmail.com>
2024-11-17 18:09:11 +0800

    Restructure the rooms folder

---
+++ fe-nuxt/pages/rooms/[id]/pre-join.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+<template>
+  <main class="h-dvh w-dvw flex flex-col justify-center items-center">
+    <UCard class="w-3/4">
+      <div class="flex flex-row">
+        <div ref="videoDiv" class="grow">Test</div>
+
+        <div class="flex flex-col">
+          <UInput />
+        </div>
+      </div>
+    </UCard>
+  </main>
+</template>
--- fe-nuxt/pages/rooms/pre-join/[id].vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>Prejoin</div>
-</template>
