470fe18271180bad16a4777f39f0281c72244c8e
Felix <jonfelixrico@gmail.com>
2024-11-17 15:35:05 +0800

    Simplify CAudioRenderer

--- fe-nuxt/components/audio/CAudioRenderer.vue
+++ fe-nuxt/components/audio/CAudioRenderer.vue
@@ -2,6 +2,13 @@
 import { useTemplateRef, watch, type PropType } from 'vue'
 
 const props = defineProps({
+  /**
+   * If the parent wishes to change mediaStream, then they have to do it in a different
+   * instance of this component, e.g. unmount then mount a new component.
+   *
+   * This component is intentionally limited to support a single `mediaStream` instance
+   * throughout its lifetime to keep it simple.
+   */
   mediaStream: {
     type: Object as PropType<MediaStream>,
     required: true,
@@ -11,13 +18,14 @@ const props = defineProps({
 const audioRef = useTemplateRef('audio')
 
 watch(
-  [audioRef, () => props.mediaStream],
-  ([audioRef, mediaStream]) => {
-    if (!audioRef || !mediaStream) {
+  audioRef,
+  (audioRef) => {
+    if (!audioRef) {
       return
     }
 
-    audioRef.srcObject = mediaStream
+    audioRef.srcObject = props.mediaStream
+    audioRef.play()
   },
   {
     immediate: true,
