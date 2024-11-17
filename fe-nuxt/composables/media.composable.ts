505fe5b9f801cfce2f49a08be65a90590e395baf
Felix <jonfelixrico@gmail.com>
2024-11-17 18:31:37 +0800

    Create media stream helper

--- fe-nuxt/composables/media.composable.ts
+++ fe-nuxt/composables/media.composable.ts
@@ -1,3 +1,4 @@
+import { useUserMedia } from '#imports'
 import { computed, toValue, type MaybeRef } from 'vue'
 import type { MaybeNullish } from '~/typings/util.types'
 
@@ -5,4 +6,37 @@ export function useHasVideo(mediaStream: MaybeRef<MaybeNullish<MediaStream>>) {
   return computed(
     () => (toValue(mediaStream)?.getVideoTracks()?.length ?? 0) > 0,
   )
+}
+
+export function useUserMediaStream(deviceIds: {
+  audio?: MaybeNullish<string>
+  video?: MaybeNullish<string>
+}) {
+  const constraints = computed<MediaStreamConstraints>(() => {
+    const value: MediaStreamConstraints = {}
+
+    if (deviceIds.audio) {
+      value.audio = {
+        deviceId: deviceIds.audio,
+      }
+    }
+
+    if (deviceIds.video) {
+      value.video = {
+        deviceId: deviceIds.video,
+      }
+    }
+
+    return value
+  })
+
+  const enabled = computed(() => Boolean(deviceIds.audio || deviceIds.video))
+
+  const { stream } = useUserMedia({
+    enabled,
+    constraints,
+    autoSwitch: true,
+  })
+
+  return stream
 }
