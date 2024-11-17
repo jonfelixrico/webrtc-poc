258b7c708f5fcdff25b348646d7748edb7939821
Felix <jonfelixrico@gmail.com>
2024-11-17 16:52:36 +0800

    Add comment

--- fe-nuxt/components/media-stream/CMediaStreamRendererVideo.vue
+++ fe-nuxt/components/media-stream/CMediaStreamRendererVideo.vue
@@ -3,6 +3,13 @@ import { useResizeObserverValue } from '~/composables/vueuse-extensions.composab
 import { useTemplateRef, watch, type PropType } from 'vue'
 
 const props = defineProps({
+  /**
+   * If the parent wishes to change mediaStream, then they have to do it in a different
+   * instance of this component, e.g. unmount then mount a new component.
+   *
+   * This component is intentionally limited to support a single `mediaStream` instance
+   * throughout its lifetime to keep it simple.
+   */
   mediaStream: {
     type: Object as PropType<MediaStream>,
     required: true,
