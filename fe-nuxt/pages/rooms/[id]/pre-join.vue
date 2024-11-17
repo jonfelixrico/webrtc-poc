8727c8d30fa6a8c1770b3aac62889365f5f05dea
Felix <jonfelixrico@gmail.com>
2024-11-17 18:53:50 +0800

    Make the pre-join functional

--- fe-nuxt/pages/rooms/[id]/pre-join.vue
+++ fe-nuxt/pages/rooms/[id]/pre-join.vue
@@ -1,11 +1,66 @@
+<script setup lang="ts">
+import { useResizeObserverValue } from '#imports'
+import { useDevicesList } from '@vueuse/core'
+import { computed, reactive, ref, useTemplateRef } from 'vue'
+import CMediaStreamRendererVideo from '~/components/media-stream/CMediaStreamRendererVideo.vue'
+import { useUserMediaStream } from '~/composables/media.composable'
+
+const { audioInputs, videoInputs } = useDevicesList({
+  constraints: {
+    audio: true,
+    video: true,
+  },
+  requestPermissions: true,
+})
+
+const audioId = ref<string>()
+const videoId = ref<string>()
+
+const mediaStream = useUserMediaStream(
+  reactive({
+    audio: audioId,
+    video: videoId,
+  }),
+)
+
+const videoDivRef = useTemplateRef('videoDiv')
+const dimensions = useResizeObserverValue(videoDivRef)
+const height = computed(() => dimensions.width * (9 / 16))
+</script>
+
 <template>
   <main class="h-dvh w-dvw flex flex-col justify-center items-center">
-    <UCard class="w-[40dvw]">
+    <UCard class="w-[50dvw]">
       <div class="flex flex-row">
-        <div ref="videoDiv" class="grow">Test</div>
+        <div ref="videoDiv" class="grow">
+          <div :style="{ height: `${height}px` }">
+            <CMediaStreamRendererVideo
+              v-if="mediaStream"
+              class="h-full w-full"
+              :media-stream
+            />
+          </div>
+        </div>
 
-        <div class="flex flex-col">
-          <UInput />
+        <div class="flex flex-col gap-2 w-56">
+          <div class="flex flex-col">
+            <!-- TODO i18nize -->
+            <div>Video</div>
+            <USelect
+              v-model="videoId"
+              :options="videoInputs"
+              value-attribute="deviceId"
+            />
+          </div>
+          <div>
+            <!-- TODO i18nize -->
+            <div>Audio</div>
+            <USelect
+              v-model="audioId"
+              :options="audioInputs"
+              value-attribute="deviceId"
+            />
+          </div>
         </div>
       </div>
     </UCard>
