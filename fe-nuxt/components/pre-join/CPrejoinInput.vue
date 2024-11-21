5b46f858f6e9918e8cd40cfb29c92a3ae530e184
Felix <jonfelixrico@gmail.com>
2024-11-21 21:23:06 +0800

    Adjust prejoin input

---
+++ fe-nuxt/components/pre-join/CPrejoinInput.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import { useUserMediaStream } from '#imports'
+import { useDevicesList } from '@vueuse/core'
+import { reactive, watch } from 'vue'
+import CMediaStreamRendererVideo from '~/components/media-stream/CMediaStreamRendererVideo.vue'
+import CPreJoinOverlay from '~/components/pre-join/CPreJoinOverlay.vue'
+import type { TrackState } from '~/typings/media-stream.types'
+
+const { audioInputs, videoInputs } = useDevicesList({
+  constraints: {
+    audio: true,
+    video: true,
+  },
+})
+
+const video = reactive<TrackState>({
+  id: null,
+  enabled: false,
+})
+watch(
+  videoInputs,
+  (newVal, oldVal) => {
+    if (!oldVal?.length && newVal.length) {
+      video.id = newVal[0].deviceId
+    }
+  },
+  {
+    once: true,
+  },
+)
+
+const audio = reactive<TrackState>({
+  id: null,
+  enabled: false,
+})
+watch(
+  audioInputs,
+  (newVal, oldVal) => {
+    if (!oldVal?.length && newVal.length) {
+      audio.id = newVal[0].deviceId
+    }
+  },
+  {
+    once: true,
+  },
+)
+
+const stream = useUserMediaStream(
+  reactive({
+    audio,
+    video,
+  }),
+)
+</script>
+
+<template>
+  <div class="relative">
+    <CMediaStreamRendererVideo
+      v-if="stream"
+      :key="stream.id"
+      :media-stream="stream"
+    />
+
+    <CPreJoinOverlay
+      v-model:audio="audio"
+      v-model:video="video"
+      class="h-full w-full absolute z-10"
+      :audio-devices="audioInputs"
+      :video-devices="videoInputs"
+    />
+  </div>
+</template>
