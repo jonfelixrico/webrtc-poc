d83146e8ce0662d4d5e521f24c4b9bfe37d4f881
Felix <jonfelixrico@gmail.com>
2024-11-21 21:49:29 +0800

    Use the prejoin input

--- fe-nuxt/pages/rooms/[id]/pre-join.vue
+++ fe-nuxt/pages/rooms/[id]/pre-join.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
 import { useDevicesList } from '@vueuse/core'
-import { reactive, ref } from 'vue'
-import CMediaStreamRendererVideo from '~/components/media-stream/CMediaStreamRendererVideo.vue'
-import { useUserMediaStream } from '~/composables/media.composable'
+import { reactive } from 'vue'
 import { definePageMeta, navigateTo } from '#imports'
 import { useRoomStore } from '~/store/room.store'
 import { useRouter } from 'vue-router'
+import type { DeviceState } from '~/typings/media.types'
+import CPrejoinInput from '~/components/pre-join/CPrejoinInput.vue'
 
 definePageMeta({
   middleware: [
@@ -38,21 +38,14 @@ const { audioInputs, videoInputs } = useDevicesList({
   requestPermissions: true,
 })
 
-const audioId = ref<string>()
-const videoId = ref<string>()
-
-const mediaStream = useUserMediaStream(
-  reactive({
-    audio: {
-      enabled: true,
-      id: audioId,
-    },
-    video: {
-      enabled: true,
-      id: videoId,
-    },
-  }),
-)
+const audio = reactive<DeviceState>({
+  id: null,
+  enabled: false,
+})
+const video = reactive<DeviceState>({
+  id: null,
+  enabled: false,
+})
 
 const { t } = useI18n()
 
@@ -68,39 +61,17 @@ function joinCall() {
 
 <template>
   <main class="h-dvh w-dvw flex flex-col justify-center items-center">
-    <UCard class="w-[50dvw]">
-      <div class="flex flex-row gap-2">
-        <div class="grow">
-          <CMediaStreamRendererVideo
-            v-if="mediaStream"
-            class="h-full w-full"
-            :media-stream
-          />
-        </div>
+    <UCard>
+      <div class="flex flex-col">
+        <CPrejoinInput
+          v-model:audio="audio"
+          v-model:video="video"
+          :audio-devices="audioInputs"
+          :video-devices="videoInputs"
+          class="w-[50dvw] h-[30dvh]"
+        />
 
-        <div class="flex flex-col justify-between gap-2 w-56">
-          <div class="flex flex-col gap-2">
-            <div class="flex flex-col">
-              <div>{{ t('preCall.video') }}</div>
-              <USelect
-                v-model="videoId"
-                :options="videoInputs"
-                value-attribute="deviceId"
-              />
-            </div>
-
-            <div class="flex flex-col">
-              <div>{{ t('preCall.audio') }}</div>
-              <USelect
-                v-model="audioId"
-                :options="audioInputs"
-                value-attribute="deviceId"
-              />
-            </div>
-          </div>
-
-          <UButton block @click="joinCall">{{ t('preCall.joinCall') }}</UButton>
-        </div>
+        <UButton block @click="joinCall">{{ t('preCall.joinCall') }}</UButton>
       </div>
     </UCard>
   </main>
