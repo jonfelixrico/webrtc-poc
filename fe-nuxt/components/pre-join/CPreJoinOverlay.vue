4d9efe1fe543b9435428da7759c3e6b76d79bd5f
Felix <jonfelixrico@gmail.com>
2024-11-21 20:00:47 +0800

    Add prejoin overlay

---
+++ fe-nuxt/components/pre-join/CPreJoinOverlay.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const videoEnabled = defineModel('videoEnabled', {
+  type: Boolean,
+})
+
+const audioEnabled = defineModel('videoEnabled', {
+  type: Boolean,
+})
+</script>
+
+<template>
+  <div class="flex flex-col justify-end">
+    <div class="flex flex-row justify-center gap-4">
+      <!-- TODO add icon -->
+      <UToggle v-model="videoEnabled" />
+
+      <!-- TODO add icon -->
+      <UToggle v-model="audioEnabled" />
+
+      <!-- TODO add icon; i18n label -->
+      <UButton variant="ghost" label="Devices" />
+    </div>
+  </div>
+</template>
