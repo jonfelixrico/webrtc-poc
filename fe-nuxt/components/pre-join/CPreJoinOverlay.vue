92317de3cb10dca5743ff52f770958ffc53ec525
Felix <jonfelixrico@gmail.com>
2024-11-21 20:43:00 +0800

    Flesh out the overlay

--- fe-nuxt/components/pre-join/CPreJoinOverlay.vue
+++ fe-nuxt/components/pre-join/CPreJoinOverlay.vue
@@ -1,11 +1,51 @@
 <script setup lang="ts">
+import { useModal } from '#imports'
+import type { PropType } from 'vue'
+import CPrejoinDevicesDialog from '~/components/pre-join/CPrejoinDevicesDialog.vue'
+
 const videoEnabled = defineModel('videoEnabled', {
   type: Boolean,
+})
+
+const videoId = defineModel('videoId', {
+  type: String,
+  default: null,
 })
 
 const audioEnabled = defineModel('videoEnabled', {
   type: Boolean,
 })
+
+const audioId = defineModel('audioId', {
+  type: String,
+  default: null,
+})
+
+const props = defineProps({
+  videoDevices: {
+    type: Array as PropType<MediaDeviceInfo[]>,
+    required: true,
+  },
+
+  audioDevices: {
+    type: Array as PropType<MediaDeviceInfo[]>,
+    required: true,
+  },
+})
+const modal = useModal()
+function openDialog() {
+  const { videoDevices, audioDevices } = props
+
+  modal.open(CPrejoinDevicesDialog, {
+    onSubmit: (v) => {
+      videoId.value = v.videoId
+      audioId.value = v.audioId
+    },
+
+    audioDevices,
+    videoDevices,
+  })
+}
 </script>
 
 <template>
@@ -18,7 +58,7 @@ const audioEnabled = defineModel('videoEnabled', {
       <UToggle v-model="audioEnabled" />
 
       <!-- TODO add icon; i18n label -->
-      <UButton variant="ghost" label="Devices" />
+      <UButton variant="ghost" label="Devices" @click="openDialog" />
     </div>
   </div>
 </template>
