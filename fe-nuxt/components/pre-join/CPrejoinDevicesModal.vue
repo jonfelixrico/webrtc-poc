fd57f15f90217fe9b21439a11dd3dfbede967b66
Felix <jonfelixrico@gmail.com>
2024-11-21 22:46:35 +0800

    Remove toValue

--- fe-nuxt/components/pre-join/CPrejoinDevicesModal.vue
+++ fe-nuxt/components/pre-join/CPrejoinDevicesModal.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useModal } from '#imports'
-import { reactive, toValue, type PropType } from 'vue'
+import { reactive, type PropType } from 'vue'
 
 const props = defineProps({
   videoDevices: {
@@ -34,7 +34,7 @@ const emit = defineEmits<{
   (e: 'submit', value: { videoId: string; audioId: string }): void
 }>()
 function confirmSelection() {
-  emit('submit', toValue(model))
+  emit('submit', model)
   modal.close()
 }
 </script>
