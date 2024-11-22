1341fa339fdd47b7abefc891108d673238dbc056
Felix <jonfelixrico@gmail.com>
2024-11-23 00:46:11 +0800

    Use NuxtLink

--- fe-nuxt/pages/index.vue
+++ fe-nuxt/pages/index.vue
@@ -1,29 +1,20 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { useRouter } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 
-const roomJoinModel = ref()
-const router = useRouter()
-
-function goToRoom(roomId: string) {
-  router.push({
-    path: `/rooms/${roomId}`,
-  })
-}
+const roomId = ref<string>()
 
 const { t } = useI18n()
 </script>
 
 <template>
   <div class="flex flex-row gap-4 w-dvw h-dvh">
-    <form
-      class="flex-1 flex flex-row items-center justify-center gap-2"
-      @submit.prevent="goToRoom(roomJoinModel)"
-    >
-      <UInput v-model="roomJoinModel" />
-      <UButton type="submit">{{ t('preCall.joinCall') }}</UButton>
-    </form>
+    <div class="flex-1 flex flex-row items-center justify-center gap-2">
+      <UInput v-model="roomId" />
+      <NuxtLink :to="`/rooms/${roomId}`">
+        <UButton type="submit">{{ t('preCall.joinCall') }}</UButton>
+      </NuxtLink>
+    </div>
 
     <div class="flex-1 flex flex-row items-center justify-center">
       <NuxtLink to="/rooms/create">
