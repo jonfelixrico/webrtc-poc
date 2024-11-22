e708df68c94544f77c99acd1d273400b034b430c
Felix <jonfelixrico@gmail.com>
2024-11-22 21:09:28 +0800

    Add strings

--- fe-nuxt/i18n/messages/en/index.ts
+++ fe-nuxt/i18n/messages/en/index.ts
@@ -1,9 +1,7 @@
-import common from './common'
 import call from './call'
 import preCall from './pre-call'
 
 export default {
-  common,
   call,
   preCall,
 }
--- fe-nuxt/i18n/messages/en/pre-call.ts
+++ fe-nuxt/i18n/messages/en/pre-call.ts
@@ -2,4 +2,8 @@ export default {
   audio: 'Audio',
   video: 'Video',
   joinCall: 'Join Call',
+  noCameras: 'No cameras found',
+  selectCamera: 'Select a camera',
+  selectMic: 'Select a mic',
+  noMic: 'No mics found',
 }
