4a7aecfe4ec379bad9c23735cc153ecea8fc2a5c
Felix <jonfelixrico@gmail.com>
2024-11-22 21:09:38 +0800

    Create input component

---
+++ fe-nuxt/components/pre-join/CPrejoinDeviceModalDeviceInput.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import type { PropType } from 'vue'
+
+const model = defineModel({
+  type: String,
+  default: null,
+})
+
+defineProps({
+  devices: {
+    type: Array as PropType<MediaDeviceInfo[]>,
+    default: () => [],
+  },
+
+  noDevicesText: {
+    type: String,
+    required: true,
+  },
+
+  placeholder: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <USelect v-if="!devices.length" disabled :placeholder="noDevicesText" />
+  <USelect
+    v-else
+    v-model="model"
+    :options="devices"
+    value-attribute="deviceId"
+    :placeholder
+  />
+</template>
