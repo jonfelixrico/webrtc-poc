d5ead9c8e58ed0db180ecf6cd6732f71f2e4a974
Felix <jonfelixrico@gmail.com>
2024-11-24 21:22:43 +0800

    Expand RoomUser, add User

--- common/src/index.ts
+++ common/src/index.ts
@@ -12,10 +12,6 @@ export interface RoomWsCommandPayloadMap {
   sync_user_list: {}
 }
 
-export interface RoomUser {
-  id: string
-}
-
 export interface RoomWsEventPayloadMap {
   candidate_sent: {
     fromClientId: string
@@ -42,3 +38,14 @@ export interface RoomWsEventPayloadMap {
 export type RoomWsEventHandler<K extends keyof RoomWsEventPayloadMap> = (
   payload: RoomWsEventPayloadMap[K],
 ) => any
+
+export interface RoomUser {
+  id: string
+  name: string
+}
+
+export interface Room {
+  id: string
+  name: string
+  users: RoomUser[]
+}
