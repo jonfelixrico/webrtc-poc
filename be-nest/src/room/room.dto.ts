3ef5381ab9a55dc8eed59d009117e99e957ab440
Felix <jonfelixrico@gmail.com>
2024-11-24 20:54:54 +0800

    Create DTO files

---
+++ be-nest/src/room/room.dto.ts
@@ -0,0 +1,14 @@
+import { Expose, Type } from 'class-transformer'
+import { IRoom, IRoomUser } from 'src/room/room.class'
+
+export class RoomUserDto implements IRoomUser {
+  @Expose() id: string
+  @Expose() name: string
+}
+
+export class RoomDto implements IRoom {
+  @Type(() => RoomUserDto)
+  @Expose()
+  users: RoomUserDto[]
+  @Expose() name: string
+}
