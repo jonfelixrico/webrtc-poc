fe120240f290d9485dfd1a648bbc2fa5232f2fa8
Felix <jonfelixrico@gmail.com>
2024-11-28 23:48:33 +0800

    Adjust id rendering

--- fe-nuxt/components/CCallRenderer.vue
+++ fe-nuxt/components/CCallRenderer.vue
@@ -12,33 +12,21 @@ import CCallAudioRenderer from '~/components/CCallAudioRenderer.vue'
 const roomStore = useRoomStore()
 
 const selfId = useUserId()
-const ids = computed(() => {
-  const ids: string[] = ['self']
-
-  for (const id in roomStore.users) {
-    // User is already identified as the first item 'self'
-    if (selfId.value === id) {
-      continue
-    }
-
-    ids.push(id)
-  }
-
-  return ids
-})
+const ids = computed(() => Object.keys(roomStore.users).sort())
 
 const msStore = useMediaStreamStore()
 const rtcStore = useWebRtcStore()
 </script>
 
 <template>
-  <CCallLayout v-slot="{ id }" v-bind="$attrs" :ids>
-    <template v-if="id === 'self'">
+  <!-- v-if is a quick-fix for now to avoid ...RendererSelf from throwing -->
+  <CCallLayout v-if="selfId" v-slot="{ id }" v-bind="$attrs" :ids>
+    <template v-if="id === selfId">
       <!-- TODO provide proper name -->
       <CCallParticipantRendererSelf
         class="h-full w-full"
-        display-name="Self"
         :media-stream="msStore.mediaStream"
+        :user="roomStore.users[id]"
       />
     </template>
 
@@ -48,7 +36,7 @@ const rtcStore = useWebRtcStore()
       <CCallParticipantRendererPeer
         :connection="rtcStore.connections.get(id)"
         class="h-full w-full"
-        :display-name="id"
+        :user="roomStore.users[id]"
       />
     </template>
   </CCallLayout>
