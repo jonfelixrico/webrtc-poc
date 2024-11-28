eb59ecc877a98508110cf362bb4d7c444917a67f
Felix <jonfelixrico@gmail.com>
2024-11-28 23:07:59 +0800

    Create the room name component

---
+++ fe-nuxt/components/pre-call/CPreCallRoomName.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useModal } from '#imports'
+import CPreCallRoomNameEditModal from '~/components/pre-call/CPreCallRoomNameEditModal.vue'
+
+const model = defineModel({
+  type: String,
+  default: '',
+})
+
+const modal = useModal()
+function openModal() {
+  modal.open(CPreCallRoomNameEditModal, {
+    onSubmit: (v) => {
+      model.value = v
+    },
+
+    value: model.value,
+  })
+}
+</script>
+
+<template>
+  <div class="gap-x-1 flex flex-row items-center justify-center">
+    <span>{{ model }}</span>
+    <!-- TODO add an actual edit icon button -->
+    <UButton @click="openModal">Edit</UButton>
+  </div>
+</template>
