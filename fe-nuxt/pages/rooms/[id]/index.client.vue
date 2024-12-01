a27905679032da27caa864d4d9fdfba7d7ee8d79
Felix <jonfelixrico@gmail.com>
2024-12-01 17:07:28 +0800

    Use the provider

--- fe-nuxt/pages/rooms/[id]/index.client.vue
+++ fe-nuxt/pages/rooms/[id]/index.client.vue
@@ -16,6 +16,7 @@ import {
 } from '~/composables/rtc-signaling-all.composable'
 import CCallRenderer from '~/components/CCallRenderer.vue'
 import { useRoomStore } from '~/store/room.store'
+import { provideUserDevices } from '~/composables/media.composable'
 
 definePageMeta({
   middleware: [
@@ -47,6 +48,7 @@ if (import.meta.client) {
   useJoinHandler()
   useDescriptionHandlers()
   useRoomMembersListener()
+  provideUserDevices()
 }
 </script>
 
