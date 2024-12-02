ccea5fbeb4d761b4938c8493b9b350b4115ce717
Felix <jonfelixrico@gmail.com>
2024-12-02 21:32:34 +0800

    Intgrate the participants list button into the toolbar

---
+++ fe-nuxt/components/call/CParticipantsList.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <UButton color="white" variant="ghost">
+    <div class="flex flex-col items-center">
+      <!-- TODO fix the icon -->
+      <UIcon name="i-ri-mic-fill" class="icon-size" />
+      {{ t('call.audioControl.mute') }}
+    </div>
+  </UButton>
+</template>
+
+<style lang="scss" scoped>
+.icon-size {
+  @apply w-7 h-7;
+}
+</style>
