e5aa41b2d6f6aea5819750394f0c6f744b2601e6
Felix <jonfelixrico@gmail.com>
2024-12-02 21:38:30 +0800

    Integrate the string and add the count

--- fe-nuxt/components/call/CParticipantsList.vue
+++ fe-nuxt/components/call/CParticipantsList.vue
@@ -1,15 +1,26 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
+import { useRoomStore } from '~/store/room.store'
+import { computed } from 'vue'
 
 const { t } = useI18n()
+
+const store = useRoomStore()
+const count = computed(() => Object.keys(store.users).length)
 </script>
 
 <template>
   <UButton color="white" variant="ghost">
-    <div class="flex flex-col items-center">
+    <div class="flex flex-col items-center relative">
       <!-- TODO fix the icon -->
       <UIcon name="i-ri-mic-fill" class="icon-size" />
-      {{ t('call.audioControl.mute') }}
+      {{ t('call.participants') }}
+
+      <div
+        class="h-full w-full absolute z-10 flex flex-row justify-end items-start"
+      >
+        {{ count }}
+      </div>
     </div>
   </UButton>
 </template>
