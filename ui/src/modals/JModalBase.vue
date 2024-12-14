b534550b020fddc952ac56a7287a2f1a3ddf0cf4
Felix <jonfelixrico@gmail.com>
2024-12-14 13:25:56 +0800

    Create modal base component

---
+++ ui/src/modals/JModalBase.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const emit = defineEmits<{
+  hide: []
+}>()
+
+function hide() {
+  emit('hide')
+}
+</script>
+
+<template>
+  <div
+    class="fixed inset-0 w-dvw h-dvh flex flex-row justify-center items-center"
+    @click.self="hide"
+  >
+    <slot :hide />
+  </div>
+</template>
