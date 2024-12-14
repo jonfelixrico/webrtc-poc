ce200f25cf9ccaece8e46531ce41d625cb7d8340
Felix <jonfelixrico@gmail.com>
2024-12-14 15:54:04 +0800

    Create JModal

---
+++ ui/src/modals/JModal.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import JModalOverlay from '@/modals/JModalOverlay.vue'
+
+const model = defineModel()
+function hide() {
+  model.value = false
+}
+</script>
+
+<template>
+  <JModalOverlay v-if="model" @hide="hide">
+    <slot :hide />
+  </JModalOverlay>
+</template>
