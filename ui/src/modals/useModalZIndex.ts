1b9a287cf36b437a2329612441beeff0a85c33f8
Felix <jonfelixrico@gmail.com>
2024-12-14 15:05:21 +0800

    Create z index manager

---
+++ ui/src/modals/useModalZIndex.ts
@@ -0,0 +1,35 @@
+import {
+  computed,
+  inject,
+  InjectionKey,
+  onUnmounted,
+  provide,
+  reactive,
+} from 'vue'
+
+const KEY: InjectionKey<symbol[]> = Symbol('modal z-index')
+
+export function useModalZIndexManager() {
+  const stack: symbol[] = reactive([])
+  provide(KEY, stack)
+}
+
+export function useModalZIndex() {
+  const stack = inject(KEY)
+
+  const localKey = Symbol()
+  stack?.push(localKey)
+
+  const index = computed(() => stack?.findIndex((item) => localKey === item))
+
+  onUnmounted(() => {
+    const idx = index.value
+    if (!stack || typeof idx === 'undefined' || idx === -1) {
+      return
+    }
+
+    stack?.splice(idx, 1)
+  })
+
+  return index
+}
