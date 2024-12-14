3e10c6a82e11f3ab023d691fcda7656740e44822
Felix <jonfelixrico@gmail.com>
2024-12-14 15:13:00 +0800

    Adjust how JModalTarget works

--- ui/src/modals/JModalTarget.vue
+++ ui/src/modals/JModalTarget.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+import { useModalZIndexManager } from '@/modals/useModalZIndex'
+
+useModalZIndexManager()
+</script>
+
 <template>
   <div data-modal-target class="fixed onset-0 isolate z-[100]" />
+  <slot />
 </template>
