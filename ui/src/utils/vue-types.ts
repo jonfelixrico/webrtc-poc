049cc95d8956f7e9917509489a10794eca48c720
Felix <jonfelixrico@gmail.com>
2024-12-15 21:56:25 +0800

    Create types

--- ui/src/utils/vue-types.ts
+++ ui/src/utils/vue-types.ts
@@ -4,3 +4,24 @@
 import { DefineComponent } from 'vue'
 
 export type SFComponent = DefineComponent<{}, {}, any>
+
+type ExtractEmitsValueType = string[] | ThisType<void>
+type ExtractEmitsValue<T extends ExtractEmitsValueType> = T extends string[]
+  ? {
+      [K in T[number]]: (...args: any[]) => void
+    }
+  : T extends ThisType<void>
+  ? {
+      [K in keyof T]: T[K]
+    }
+  : never
+
+type ToOnFormat<T extends string> = `on${Capitalize<T>}`
+
+type PrefixWithOn<T extends {}> = {
+  [K in keyof T as ToOnFormat<K extends string ? K : never>]: T[K]
+}
+
+type ExtractEmits<T extends SFComponent> = PrefixWithOn<
+  ExtractEmitsValue<Exclude<T['emits'], undefined>>
+>
