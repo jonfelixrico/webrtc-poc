83f78e1fd21f113e18708d87c5b514cf218b113e
Felix <jonfelixrico@gmail.com>
2024-12-17 21:18:31 +0800

    Move the brains of the operation to its own composable

---
+++ ui/src/modal/JProgrammaticModalsRenderer.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import JProgrammaticModalContainer from '@/modal/JProgrammaticModalContainer.vue'
+import { useProgrammaticModalState } from '@/modal/useProgrammaticModal'
+import { computed } from 'vue'
+
+const state = useProgrammaticModalState()
+
+const asArray = computed(() => {
+  const entries = Array.from(state.entries())
+  return entries.map(([id, value]) => ({
+    id,
+    ...value,
+  }))
+})
+</script>
+
+<template>
+  <JProgrammaticModalContainer
+    v-for="{ id, component, toBind, onHide } in asArray"
+    :key="id"
+    :dialog-component="component"
+    :to-bind
+    @hide="onHide"
+  />
+</template>
---
+++ ui/src/modal/useProgrammaticModal.ts
@@ -0,0 +1,62 @@
+import { ExtractPropAndEmitTypes, SFComponent } from '@/utils/vue-types'
+import { inject, InjectionKey, provide, reactive, readonly } from 'vue'
+
+interface ModalEntry {
+  component: SFComponent
+  toBind: ExtractPropAndEmitTypes<SFComponent>
+  onHide: () => void
+}
+
+export interface ProgrammaticModalActions {
+  open<T extends SFComponent>(
+    component: T,
+    options: {
+      toBind?: ExtractPropAndEmitTypes<T>
+    },
+  ): void
+}
+
+const STATE_KEY: InjectionKey<Map<symbol, ModalEntry>> = Symbol(
+  'programmatic modal state',
+)
+
+const ACTIONS_KEY: InjectionKey<ProgrammaticModalActions> = Symbol(
+  'programmatic modal open',
+)
+
+export function useProvideProgrammaticModalManager() {
+  const modals: Map<symbol, ModalEntry> = reactive(new Map())
+  provide(STATE_KEY, modals)
+
+  provide(ACTIONS_KEY, {
+    open<T extends SFComponent>(
+      component: T,
+      options: { toBind?: ExtractPropAndEmitTypes<T> },
+    ) {
+      const id = Symbol()
+      modals.set(id, {
+        component,
+        toBind: options?.toBind ?? {},
+        onHide: () => modals.delete(id),
+      })
+    },
+  })
+}
+
+export function useProgrammaticModal() {
+  const actions = inject(ACTIONS_KEY)
+  if (!actions) {
+    throw new Error('no modal open provided')
+  }
+
+  return actions.open
+}
+
+export function useProgrammaticModalState() {
+  const state = inject(STATE_KEY)
+  if (!state) {
+    throw new Error('no modal state provided')
+  }
+
+  return readonly(state)
+}
