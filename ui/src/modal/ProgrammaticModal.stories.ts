e30a4ad71ff64db971433af619aa9d59c1a7c47d
Felix <jonfelixrico@gmail.com>
2024-12-18 20:21:31 +0800

    Remove unused imports

--- ui/src/modal/ProgrammaticModal.stories.ts
+++ ui/src/modal/ProgrammaticModal.stories.ts
@@ -1,4 +1,4 @@
-import type { ArgTypes, Meta, StoryObj } from '@storybook/vue3'
+import type { Meta, StoryObj } from '@storybook/vue3'
 import Target from './JModalTarget.vue'
 import { useBodyScrollManager } from '@/utils/useBodyScroll'
 import {
