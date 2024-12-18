a72a9357530f4b87066bd0c59afced72d5b1d8ea
Felix <jonfelixrico@gmail.com>
2024-12-18 19:39:27 +0800

    Create test modal

---
+++ ui/src/modal/DemoModal.storybook.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import JModal from '@/modal/JModal.vue'
+</script>
+
+<template>
+  <JModal v-slot="{ hide }">
+    <button @click="hide">Close</button>
+  </JModal>
+</template>
