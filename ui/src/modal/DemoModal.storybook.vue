da376c3825e0c8dcbfb905e46c4f125e21d87109
Felix <jonfelixrico@gmail.com>
2024-12-18 20:22:03 +0800

    Make DemoModal open itself

--- ui/src/modal/DemoModal.storybook.vue
+++ ui/src/modal/DemoModal.storybook.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
 import JModal from '@/modal/JModal.vue'
+import { useProgrammaticModalOpen } from '@/modal/useProgrammaticModal'
+import DemoModal from '@/modal/DemoModal.storybook.vue'
+
+const open = useProgrammaticModalOpen()
+function recursivelyOpen() {
+  open(DemoModal)
+}
 </script>
 
 <template>
   <JModal v-slot="{ hide }">
-    <button @click="hide">Close</button>
+    <div class="flex flex-col gap-y-2 p-2 bg-white">
+      <button @click="hide">Close</button>
+      <button @click="recursivelyOpen">Open</button>
+    </div>
   </JModal>
 </template>
