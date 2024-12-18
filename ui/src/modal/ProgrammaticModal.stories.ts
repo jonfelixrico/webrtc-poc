30a20670835e47e327928f9e1f475def84d9921f
Felix <jonfelixrico@gmail.com>
2024-12-18 19:53:24 +0800

    Create stories for the programmatic modal

---
+++ ui/src/modal/ProgrammaticModal.stories.ts
@@ -0,0 +1,61 @@
+import type { ArgTypes, Meta, StoryObj } from '@storybook/vue3'
+import Target from './JModalTarget.vue'
+import { useBodyScrollManager } from '@/utils/useBodyScroll'
+import {
+  useProgrammaticModalOpen,
+  useProvideProgrammaticModalManager,
+} from './useProgrammaticModal'
+import Renderer from './JProgrammaticModalsRenderer.vue'
+import DemoModal from './DemoModal.storybook.vue'
+
+const meta: Meta = {
+  decorators: [
+    (story) => {
+      return {
+        components: { story, Target, Renderer },
+
+        setup() {
+          useBodyScrollManager()
+          useProvideProgrammaticModalManager()
+        },
+
+        template: `
+          <Target>
+            <story />  
+            <Renderer />
+          </Target>
+        `,
+      }
+    },
+  ],
+  parameters: {
+    layout: 'fullscreen',
+  },
+}
+export default meta
+
+type Story = StoryObj
+
+export const Defualt: Story = {
+  args: {
+    default: 'Content',
+    modelValue: false,
+  },
+
+  render: () => ({
+    setup() {
+      const open = useProgrammaticModalOpen()
+      function openModal() {
+        open(DemoModal)
+      }
+
+      return {
+        openModal,
+      }
+    },
+
+    template: `
+      <button @click="openModal">Open a modal</button>
+    `,
+  }),
+}
