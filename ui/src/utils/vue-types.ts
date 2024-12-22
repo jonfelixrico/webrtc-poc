61838dc656fffcd9ea186100494379088f716030
Felix <jonfelixrico@gmail.com>
2024-12-22 17:25:47 +0800

    fix: prevent the bind type from requiring things to be added

--- ui/src/utils/vue-types.ts
+++ ui/src/utils/vue-types.ts
@@ -9,13 +9,15 @@ type EmitToEmitBindNotation<T> = T extends (
   ? { [K in E & string as PrefixWithOn<K>]: (...args: A) => R }
   : never
 
-export type BindTypes<T extends Component> = ComponentProps<T> &
-  /*
-   * We can't use ComponentEmit as is because it produces a type of something like
-   * (event: 'foo', ...args: any[]): void && (event: 'bar', ...args: any[]): void;
-   * which produces { foo: (...args: any[]) => void), bar: (...args: any[]) => void) }.
-   *
-   * BindTypes are intended to be v-binded into a component; emits in v-binds are prefixed with
-   * `on*`, e.g. `onFoo`, `onBar`. EmitToEmitBindNotation does this conversion for us.
-   */
-  EmitToEmitBindNotation<ComponentEmit<T>>
+export type BindTypes<T extends Component> = Partial<
+  ComponentProps<T> &
+    /*
+     * We can't use ComponentEmit as is because it produces a type of something like
+     * (event: 'foo', ...args: any[]): void && (event: 'bar', ...args: any[]): void;
+     * which produces { foo: (...args: any[]) => void), bar: (...args: any[]) => void) }.
+     *
+     * BindTypes are intended to be v-binded into a component; emits in v-binds are prefixed with
+     * `on*`, e.g. `onFoo`, `onBar`. EmitToEmitBindNotation does this conversion for us.
+     */
+    EmitToEmitBindNotation<ComponentEmit<T>>
+>
